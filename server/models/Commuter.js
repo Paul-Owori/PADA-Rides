5bbb6fe17a6eec44e2230690d144e361a5d892a1
Aisha <kiwukanaishah@gmail.com>
2019-09-25 08:38:51 -0700

    added schema models

---
+++ server/models/Commuter.js
@@ -0,0 +1,26 @@
+commuter = {
+    id : 80,
+    firstName : "",
+    lastName : "",
+    userName : "",
+    passWord : "",
+    contact : {
+        email : "",
+        telephone : 9,
+    },
+    pwdStatus : true,
+        impairments : {
+            physical: {
+                crachUser : true,
+                wheelchairUser : true,
+            },
+            visual : {
+                partial : true,
+                total : true,
+            },
+            hearing : {
+                partial : true,
+                total : true,
+            },
+        }
+    }
