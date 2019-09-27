d00e8ba63ee2dc627ae0f22e33da8b01d9ece830
Paul-Owori <prowori2@gmail.com>
2019-09-27 09:49:12 +0300

    added data gathering for sp and commuter profile pages

---
+++ client/public/scripts/data-control/sp-profile.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    // Handling image uploads
+
+
+    //Handling submitting details
+    $('#saveChanges').click(() => {
+
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let email = $('#email').val();
+        let phoneNumber = $('#phoneNumber').val();
+        let bio = $('#bio').val();
+
+
+
+    })
+})
